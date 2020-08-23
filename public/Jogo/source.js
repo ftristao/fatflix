8821332dc132b08043d9a7fa048f70386de51a74
ftristao <ftristao@bol.com.br>
2020-08-23 10:52:47 -0300

    Ajustes no Jogo

--- public/Jogo/source.js
+++ public/Jogo/source.js
@@ -5,6 +5,7 @@
 /* eslint-disable no-plusplus */
 let turn = 'X';
 let board = [[], [], []];
+let nivel = 0;
 // muda o turno
 function switchTurn() {
   turn = (turn === 'X') ? 'círculo' : 'X';
@@ -60,47 +61,107 @@ function createTags() {
 //
 function minhaVez(l, c) {
   let minhasChances = [];
+  let minhasChancesV = [];
+  let minhasChancesH = [];
+  let minhasChancesD1 = [];
+  let minhasChancesD2 = [];
+  let fullTest = 1;
   const posAnterior = `${l}${c}`;
   switchTurn();
-  switch (posAnterior) {
-    case '00':
-      minhasChances = ['01', '02', '10', '20', '11', '22'];
-      break;
-    case '01':
-      minhasChances = ['00', '02', '11', '22'];
-      break;
-    case '02':
-      minhasChances = ['01', '02', '12', '22'];
-      break;
-    case '10':
-      minhasChances = ['00', '20', '11', '12'];
-      break;
-    case '11':
-      minhasChances = ['10', '12', '00', '22', '20', '02'];
-      break;
-    case '12':
-      minhasChances = ['10', '11', '02', '22'];
-      break;
-    case '20':
-      minhasChances = ['10', '00', '21', '22', '11', '02'];
-      break;
-    case '21':
-      minhasChances = ['20', '22', '11', '01'];
-      break;
-    default:
-      minhasChances = ['20', '21', '12', '02'];
+  if (nivel === 1) {
+    if (board[1][1] == null) {
+      document.getElementById('11').querySelector('img').src = 'img/o.png';
+      board[1][1] = turn;
+      fullTest = 0;
+    }
   }
-  for (let n = 0; n < minhasChances.length; n++) {
-    const a = minhasChances[n].split('')[0];
-    const b = minhasChances[n].split('')[1];
-    if (board[a][b] === 'círculo' || board[a][b] === 'X') minhasChances.splice(n, 1);
+  if (fullTest) {
+    switch (posAnterior) {
+      case '00':
+        minhasChancesH = ['01', '02'];
+        minhasChancesV = ['10', '20'];
+        minhasChancesD1 = ['11', '22'];
+        break;
+      case '01':
+        minhasChancesH = ['00', '02'];
+        minhasChancesV = ['11', '22'];
+        break;
+      case '02':
+        minhasChancesH = ['00', '01'];
+        minhasChancesV = ['12', '22'];
+        break;
+      case '10':
+        minhasChancesH = ['00', '20'];
+        minhasChancesV = ['11', '12'];
+        break;
+      case '11':
+        minhasChancesH = ['10', '12'];
+        minhasChancesV = ['01', '21'];
+        minhasChancesD1 = ['00', '22'];
+        minhasChancesD2 = ['20', '02'];
+        break;
+      case '12':
+        minhasChancesH = ['10', '11'];
+        minhasChancesV = ['02', '22'];
+        break;
+      case '20':
+        minhasChancesH = ['10', '00'];
+        minhasChancesV = ['21', '22'];
+        minhasChancesD1 = ['11', '02'];
+        break;
+      case '21':
+        minhasChancesH = ['20', '22'];
+        minhasChancesV = ['11', '01'];
+        break;
+      default:
+        minhasChancesH = ['20', '21'];
+        minhasChancesV = ['12', '02'];
+        minhasChancesD1 = ['00', '11'];
+    }
+    // Elimina impossibilidades na horizontal
+    for (let n = 0; n < minhasChancesH.length; n++) {
+      const a = minhasChancesH[n].split('')[0];
+      const b = minhasChancesH[n].split('')[1];
+      if (board[a][b] === 'círculo') minhasChancesH = [];
+      if (board[a][b] === 'X') minhasChancesH.splice(n, 1);
+    }
+    // Elimina impossibilidades na vertical
+    for (let n = 0; n < minhasChancesV.length; n++) {
+      const a = minhasChancesV[n].split('')[0];
+      const b = minhasChancesV[n].split('')[1];
+      if (board[a][b] === 'X') minhasChancesV.splice(n, 1);
+      if (board[a][b] === 'círculo') minhasChancesV = [];
+    }
+    // Elimina impossibilidades na Diagonal1
+    for (let n = 0; n < minhasChancesD1.length; n++) {
+      const a = minhasChancesD1[n].split('')[0];
+      const b = minhasChancesD1[n].split('')[1];
+      if (board[a][b] === 'X') minhasChancesD1.splice(n, 1);
+      if (board[a][b] === 'círculo') minhasChancesD1 = [];
+    }
+    // Elimina impossibilidades na Diagonal2
+    for (let n = 0; n < minhasChancesD2.length; n++) {
+      const a = minhasChancesD2[n].split('')[0];
+      const b = minhasChancesD2[n].split('')[1];
+      if (board[a][b] === 'X') minhasChancesD2.splice(n, 1);
+      if (board[a][b] === 'círculo') minhasChancesD2 = [];
+    }
+    // Agrupa possibilidades restantes
+    minhasChances = [...minhasChancesH, ...minhasChancesV, ...minhasChancesD1, ...minhasChancesD2];
+    // Caso não tenha sobrado nada, recria matriz com todas as possibilidades
+    if (minhasChances.length === 0) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (board[i][j] == null) minhasChances.push(`${i}${j}`);
+        }
+      }
+    }
+    const randomPos = Math.floor(Math.random() * (minhasChances.length - 1));
+    const i = minhasChances[randomPos].split('')[0];
+    const j = minhasChances[randomPos].split('')[1];
+    document.getElementById(`${i}${j}`).querySelector('img').src = 'img/o.png';
+    board[i][j] = turn;
   }
-
-  const randomPos = Math.floor(Math.random() * (minhasChances.length - 1));
-  const i = minhasChances[randomPos].split('')[0];
-  const j = minhasChances[randomPos].split('')[1];
-  document.getElementById(`${i}${j}`).querySelector('img').src = 'img/o.png';
-  board[i][j] = turn;
   const hasWon = verifyWin();
   if (hasWon || verifyTie()) {
     createTags();
@@ -119,7 +180,6 @@ function move(element) {
       if (hasWon || verifyTie()) {
         createTags();
         document.body.querySelector('div#result').innerHTML = (hasWon) ? `O ${turn} venceu!!` : 'O jogo empatou!!';
-        turn = 'X';
       } else minhaVez(i, j);
     }
   }
@@ -127,9 +187,11 @@ function move(element) {
 // reseta o jogo
 function restart() {
   board = [[], [], []];
+  turn = 'X';
+  nivel = 1;
   const imgs = document.getElementsByTagName('img');
   // começar do 1 pois o elemento 0 countém a imagem que diz a vez
-  for (let i = 1; i < imgs.length; i++) { imgs[i].src = ''; }
+  for (let i = 1; i < imgs.length; i++) { imgs[i].src = 'img/n.png'; }
   const button = document.querySelector('input.btn');
   const result = document.querySelector('div#result');
   document.body.querySelector('section#interface').removeChild(result);
